a4be0e6b9df6505e90cff54fc683bbdb3efd7934
johnaopedro <87997969+johnaopedro@users.noreply.github.com>
2024-04-12 21:26:47 -0300

    Update Is_GraphBipartite.py
    BFS

--- Is_Graph_Bipartite/Is_GraphBipartite.py
+++ Is_Graph_Bipartite/Is_GraphBipartite.py
@@ -1,4 +1,25 @@
+#BFS(G)
+#for every vertex s of G not explored yet
+#do Enqueue(S,s);
+#mark vertex s as visited
+#while S is not empty do
+#u ← Dequeue(S);
+#For each v in Adj[u] then
+#if v is unexplored then
+#mark edge (v,u) as tree edge
+#mark vertex v as visited
+#Enqueue(S,v)
+
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n=len(graph)
         vetor=[0]*n
+        for no in range(n):
+            if vetor[no]==0: #Começando apenas se não for colorido
+                fila=[(no, 1)]
+                i=0
+                while (i<len(fila)):
+                    atual, cor =fila[i]
+                    i =i+1
+                    if (vetor[atual]==0):
+                        vetor[atual]=cor  #Pintando
