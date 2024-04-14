0ea4d3bd0cef5d73cb0fbbd345178ae53438886d
Tiago1604 <202028973@aluno.unb.br>
2024-04-14 01:00:29 -0300

    Segunda versão swin-in-rising-water

--- swim-in-rising-water/swim-in-rising-water.py
+++ swim-in-rising-water/swim-in-rising-water.py
@@ -1,12 +1,27 @@
-class Solution(object):
+class Solution:
     def swimInWater(self, grid):
         n = len(grid)
-        visitados = set()
+        movimentos = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        top = max(2 * (n - 1), grid[0][0], grid[n - 1][n - 1])
+        min_val = n * n - 1
+        while top <= min_val:
+            mid = (top + min_val) // 2
+            visitados = set()
+            if self.dfs(grid, visitados, 0, 0, mid):
+                min_val = mid - 1
+            else:
+                top = mid + 1
+        return top
 
-    def visitar_quadrado(grid, visitados, i, j, lvl = 0):
-        if 0 <= i < n or 0 <= j < n or (i,j) not in visitados:
-            if(i == n-1 and j == n-1): return max(lvl,grid[i][j])
-            visitados.add((i,j))
-            lvl = max(lvl,grid[i][j])
-            return min(visitar_quadrado(grid,visitados,i+1,j,lvl),visitar_quadrado(grid,visitados,i,j+1,lvl),visitar_quadrado(grid,visitados,i-1,j,lvl),visitar_quadrado(grid,visitados,i,j-1,lvl))
-        
+    def dfs(self, grid, visitados, i, j, w_lvl):
+        n = len(grid)
+        if i < 0 or j < 0 or i >= n or j >= n or (i, j) in visitados or grid[i][j] > w_lvl:
+            return False
+        if i == n - 1 and j == n - 1:
+            return True
+        visitados.add((i, j))
+        movimentos = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for movimento in movimentos:
+            if self.dfs(grid, visitados, i + movimento[0], j + movimento[1], w_lvl):
+                return True
+        return False
