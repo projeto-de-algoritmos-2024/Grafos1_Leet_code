3664a0110fab71e6fca580e2ecf9609724d1560f
John <johnaopedro@gmail.com>
2024-04-14 23:26:44 -0300

    Imagem e codigo
    Co-authored-by: Tiago1604 <202028973@aluno.unb.br>

--- Shortest_Path_Visiting_All_Nodes/Shortest_Path_Visiting_All_Nodes.py
+++ Shortest_Path_Visiting_All_Nodes/Shortest_Path_Visiting_All_Nodes.py
@@ -1,8 +1,3 @@
-class Solution:
-    def shortestPathLength(self, graph: List[List[int]]) -> int:
-      n = len(graph)
-          
-        
 #Testar uma BFS para encontrar o caminho mais curto.
   #BFS(G)
       #for every vertex s of G not explored yet
@@ -16,4 +11,24 @@ class Solution:
                   #mark vertex v as visited
                   #Enqueue(S,v)
 
-    return #Se solução for encontrada
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph) #Conta tudo dentro.
+        todosnos = (2**n) - 1  #Valor de quando os nos são visitados
+        visitados = set()  # Conjunto para armazenar os estados visitados
+        fila = [] #Cria fila
+        for i in range(n): #Inicializando fila com nós iniciais
+            fila.append((i, 2**i))     
+        aresta = 0  #Contador de arestas andadas       
+        while fila:
+            tamanho = len(fila)
+            for x in range(0, tamanho):
+                no, estado = fila.pop(0)
+                if todosnos==estado:
+                    return aresta
+                if (no,estado) in visitados:continue
+                visitados.add((no,estado))
+                for vizinho in graph[no]:
+                    fila.append((vizinho, estado | (2**vizinho))) 
+            aresta=aresta+1
+        return -1
